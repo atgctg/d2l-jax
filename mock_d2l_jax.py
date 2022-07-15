c772ab28dd5d08fb76de629636c76d9ffd666de7
atgctg <105969161+atgctg@users.noreply.github.com>
2022-07-15 16:22:55 +0200

    3.1-3.3

--- mock_d2l_jax.py
+++ mock_d2l_jax.py
@@ -1,14 +1,21 @@
+import sys
+import collections
+import time
+import inspect
+from IPython import display
+
 from matplotlib_inline import backend_inline
 import matplotlib.pyplot as plt
-import time
+
+import numpy as np
 import jax
 from jax import random, numpy as jnp
-import torch
-from torch.utils import data
-import numpy as np
+from flax import linen as nn
+import optax
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
+d2l = sys.modules[__name__]
 
 def use_svg_display():
     """Use the svg format to display a plot in Jupyter.
@@ -89,128 +96,287 @@ def plot(
     set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
 
 
-# 3.1
-class Timer:  # @save
-    """Record multiple running times."""
-
-    def __init__(self):
-        self.times = []
-        self.start()
-
-    def start(self):
-        """Start the timer."""
-        self.tik = time.time()
-
-    def stop(self):
-        """Stop the timer and record the time in a list."""
-        self.times.append(time.time() - self.tik)
-        return self.times[-1]
-
-    def avg(self):
-        """Return the average time."""
-        return sum(self.times) / len(self.times)
-
-    def sum(self):
-        """Return the sum of time."""
-        return sum(self.times)
-
-    def cumsum(self):
-        """Return the accumulated time."""
-        return np.array(self.times).cumsum().tolist()
-
 
 # 3.2
 
-def synthetic_data(w, b, num_examples, key):  # @save
-    """Generate y = Xw + b + noise."""
-    key1, key2 = random.split(key)
-    X = random.normal(key1, (num_examples, len(w))) * 1 + 0
-    y = jnp.matmul(X, w) + b
-    y += random.normal(key2, y.shape) * 0.01 + 0
-    return X, y.reshape((-1, 1))
+def add_to_class(Class):  # @save
+    def wrapper(obj):
+        setattr(Class, obj.__name__, obj)
+
+    return wrapper
 
 
-def linreg(X, w, b):  # @save
-    """The linear regression model."""
-    return jnp.matmul(X, w) + b
+class HyperParameters:  # @save
+    def save_hyperparameters(self, ignore=[]):
+        """Save function arguments into class attributes."""
+        frame = inspect.currentframe().f_back
+        _, _, _, local_vars = inspect.getargvalues(frame)
+        self.hparams = {
+            k: v
+            for k, v in local_vars.items()
+            if k not in set(ignore + ["self"]) and not k.startswith("_")
+        }
+        for k, v in self.hparams.items():
+            setattr(self, k, v)
 
+class ProgressBoard(d2l.HyperParameters):
+    """Plot data points in animation.
 
-def squared_loss(y_hat, y):  # @save
-    """Squared loss."""
-    return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
+    Defined in :numref:`sec_oo-design`"""
+    def __init__(self, xlabel=None, ylabel=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 ls=['-', '--', '-.', ':'], colors=['C0', 'C1', 'C2', 'C3'],
+                 fig=None, axes=None, figsize=(3.5, 2.5), display=True):
+        self.save_hyperparameters()
 
-@jax.jit
-def sgd(params, grads, lr):  # @save
-    """Minibatch stochastic gradient descent."""
-    params = jax.tree_util.tree_map(lambda p, g: p - lr * g, params, grads)
-    return params
+    def draw(self, x, y, label, every_n=1):
+        raise NotImplemented
 
+    def draw(self, x, y, label, every_n=1):
+        """Defined in :numref:`sec_utils`"""
+        Point = collections.namedtuple('Point', ['x', 'y'])
+        if not hasattr(self, 'raw_points'):
+            self.raw_points = collections.OrderedDict()
+            self.data = collections.OrderedDict()
+        if label not in self.raw_points:
+            self.raw_points[label] = []
+            self.data[label] = []
+        points = self.raw_points[label]
+        line = self.data[label]
+        points.append(Point(x, y))
+        if len(points) != every_n:
+            return
+        mean = lambda x: sum(x) / len(x)
+        line.append(Point(mean([p.x for p in points]),
+                          mean([p.y for p in points])))
+        points.clear()
+        if not self.display:
+            return
+        d2l.use_svg_display()
+        if self.fig is None:
+            self.fig = d2l.plt.figure(figsize=self.figsize)
+        plt_lines, labels = [], []
+        for (k, v), ls, color in zip(self.data.items(), self.ls, self.colors):
+            plt_lines.append(d2l.plt.plot([p.x for p in v], [p.y for p in v],
+                                          linestyle=ls, color=color)[0])
+            labels.append(k)
+        axes = self.axes if self.axes else d2l.plt.gca()
+        if self.xlim: axes.set_xlim(self.xlim)
+        if self.ylim: axes.set_ylim(self.ylim)
+        if not self.xlabel: self.xlabel = self.x
+        axes.set_xlabel(self.xlabel)
+        axes.set_ylabel(self.ylabel)
+        axes.set_xscale(self.xscale)
+        axes.set_yscale(self.yscale)
+        axes.legend(plt_lines, labels)
+        display.display(self.fig)
+        display.clear_output(wait=True)
+
+class Module(nn.Module, d2l.HyperParameters):
+    """Defined in :numref:`sec_oo-design`"""
+    def __init__(self, plot_train_per_epoch=2, plot_valid_per_epoch=1):
+        super().__init__()
+        self.save_hyperparameters()
+        self.board = ProgressBoard()
+        self.training = None
+
+    def loss(self, y_hat, y):
+        raise NotImplementedError
+
+    def forward(self, X):
+        assert hasattr(self, 'net'), 'Neural network is defined'
+        return self.net(X)
+
+    def call(self, X, *args, **kwargs):
+        if kwargs and "training" in kwargs:
+            self.training = kwargs['training']
+        return self.forward(X, *args)
+
+    def plot(self, key, value, train):
+        """Plot a point in animation."""
+        assert hasattr(self, 'trainer'), 'Trainer is not inited'
+        self.board.xlabel = 'epoch'
+        if train:
+            x = self.trainer.train_batch_idx / \
+                self.trainer.num_train_batches
+            n = self.trainer.num_train_batches / \
+                self.plot_train_per_epoch
+        else:
+            x = self.trainer.epoch + 1
+            n = self.trainer.num_val_batches / \
+                self.plot_valid_per_epoch
+        self.board.draw(x, value, (
+            'train_' if train else 'val_') + key, every_n=int(n))
+    def training_step(self, batch):
+        l = self.loss(self(*batch[:-1]), batch[-1])
+        self.plot('loss', l, train=True)
+        return l
+
+    def validation_step(self, batch):
+        l = self.loss(self(*batch[:-1]), batch[-1])
+        self.plot('loss', l, train=False)
+
+    def configure_optimizers(self):
+        raise NotImplementedError
+
+    def configure_optimizers(self):
+        """Defined in :numref:`sec_classification`"""
+        return optax.sgd(learning_rate=self.lr)
+
+class DataModule(d2l.HyperParameters):
+    """Defined in :numref:`sec_oo-design`"""
+    def __init__(self, root='../data'):
+        self.save_hyperparameters()
+
+    def get_dataloader(self, train):
+        raise NotImplementedError
+
+    def train_dataloader(self):
+        return self.get_dataloader(train=True)
+
+    def val_dataloader(self):
+        return self.get_dataloader(train=False)
+
+    def get_tensorloader(self, tensors, train, indices=slice(0, None)):
+        """Defined in :numref:`sec_synthetic-regression-data`"""
+        tensors = tuple(a[indices] for a in tensors)
+        shuffle_buffer = tensors[0].shape[0] if train else 1
+        return tfds.as_numpy(tf.data.Dataset.from_tensor_slices(tensors).shuffle(
+            buffer_size=shuffle_buffer).batch(self.batch_size))
+    
+
+class Trainer(d2l.HyperParameters):
+    """Defined in :numref:`sec_oo-design`"""
+    def __init__(self, max_epochs, num_gpus=0, gradient_clip_val=0):
+        self.save_hyperparameters()
+        assert num_gpus == 0, 'No GPU support yet'
+
+    def prepare_data(self, data):
+        self.train_dataloader = data.train_dataloader()
+        self.val_dataloader = data.val_dataloader()
+        self.num_train_batches = len(self.train_dataloader)
+        self.num_val_batches = (len(self.val_dataloader)
+                                if self.val_dataloader is not None else 0)
+
+    def prepare_model(self, model):
+        model.trainer = self
+        model.board.xlim = [0, self.max_epochs]
+        self.model = model
+
+    def fit(self, model, data):
+        self.prepare_data(data)
+        self.prepare_model(model)
+        self.optim = model.configure_optimizers()
+        self.epoch = 0
+        self.train_batch_idx = 0
+        self.val_batch_idx = 0
+        for self.epoch in range(self.max_epochs):
+            self.fit_epoch()
+
+    def fit_epoch(self):
+        raise NotImplementedError
+
+    def prepare_batch(self, batch):
+        """Defined in :numref:`sec_linear_scratch`"""
+        return batch
+
+    # todo + oo-design
+    def fit_epoch(self):
+        """Defined in :numref:`sec_linear_scratch`"""
+        self.model.training = True
+        for batch in self.train_dataloader:
+            with tf.GradientTape() as tape:
+                loss = self.model.training_step(self.prepare_batch(batch))
+            grads = tape.gradient(loss, self.model.trainable_variables)
+            if self.gradient_clip_val > 0:
+                grads = self.clip_gradients(self.gradient_clip_val, grads)
+            self.optim.apply_gradients(zip(grads, self.model.trainable_variables))
+            self.train_batch_idx += 1
+        if self.val_dataloader is None:
+            return
+        self.model.training = False
+        for batch in self.val_dataloader:
+            self.model.validation_step(self.prepare_batch(batch))
+            self.val_batch_idx += 1
+
+    # todo
+    def clip_gradients(self, grad_clip_val, grads):
+        """Defined in :numref:`sec_rnn-scratch`"""
+        grad_clip_val = tf.constant(grad_clip_val, dtype=tf.float32)
+        new_grads = [tf.convert_to_tensor(grad) if isinstance(
+            grad, tf.IndexedSlices) else grad for grad in grads]
+        norm = tf.math.sqrt(sum((tf.reduce_sum(grad ** 2)) for grad in new_grads))
+        if tf.greater(norm, grad_clip_val):
+            for i, grad in enumerate(new_grads):
+                new_grads[i] = grad * grad_clip_val / norm
+            return new_grads
+        return grads
 
 # 3.3
 
 # Since JAX doesn't have a dataloader, we'll use PyTorch's
-def load_array(data_arrays, batch_size, is_train=True):  # @save
-    """Construct a PyTorch data iterator."""
-    # convert JAX arrays to PyTorch tensors
-    data_arrays = (torch.from_numpy(np.array(x)) for x in data_arrays)
+# def load_array(data_arrays, batch_size, is_train=True):  # @save
+#     """Construct a PyTorch data iterator."""
+#     # convert JAX arrays to PyTorch tensors
+#     data_arrays = (torch.from_numpy(np.array(x)) for x in data_arrays)
 
-    dataset = data.TensorDataset(*data_arrays)
-    return data.DataLoader(dataset, batch_size, shuffle=is_train)
+#     dataset = data.TensorDataset(*data_arrays)
+#     return data.DataLoader(dataset, batch_size, shuffle=is_train)
 
 
 # 3.5
 
-def get_fashion_mnist_labels(labels):  # @save
-    """Return text labels for the Fashion-MNIST dataset."""
-    text_labels = [
-        "t-shirt",
-        "trouser",
-        "pullover",
-        "dress",
-        "coat",
-        "sandal",
-        "shirt",
-        "sneaker",
-        "bag",
-        "ankle boot",
-    ]
-    return [text_labels[int(i)] for i in labels]
+# def get_fashion_mnist_labels(labels):  # @save
+#     """Return text labels for the Fashion-MNIST dataset."""
+#     text_labels = [
+#         "t-shirt",
+#         "trouser",
+#         "pullover",
+#         "dress",
+#         "coat",
+#         "sandal",
+#         "shirt",
+#         "sneaker",
+#         "bag",
+#         "ankle boot",
+#     ]
+#     return [text_labels[int(i)] for i in labels]
 
 
-def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):  # @save
-    """Plot a list of images."""
-    figsize = (num_cols * scale, num_rows * scale)
-    _, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
-    axes = axes.flatten()
-    for i, (ax, img) in enumerate(zip(axes, imgs)):
-        ax.imshow(img)
-        ax.axes.get_xaxis().set_visible(False)
-        ax.axes.get_yaxis().set_visible(False)
-        if titles:
-            ax.set_title(titles[i])
-    return axes
+# def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):  # @save
+#     """Plot a list of images."""
+#     figsize = (num_cols * scale, num_rows * scale)
+#     _, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+#     axes = axes.flatten()
+#     for i, (ax, img) in enumerate(zip(axes, imgs)):
+#         ax.imshow(img)
+#         ax.axes.get_xaxis().set_visible(False)
+#         ax.axes.get_yaxis().set_visible(False)
+#         if titles:
+#             ax.set_title(titles[i])
+#     return axes
 
 
-def load_data_fashion_mnist(batch_size, resize=None):  # @save
-    """Download the Fashion-MNIST dataset and then load it into memory."""
-    mnist_train, mnist_test = tf.keras.datasets.fashion_mnist.load_data()
-    # Divide all numbers by 255 so that all pixel values are between
-    # 0 and 1, add a batch dimension at the last. And cast label to int32
-    process = lambda X, y: (tf.expand_dims(X, axis=3) / 255, tf.cast(y, dtype="int32"))
-    resize_fn = lambda X, y: (
-        tf.image.resize_with_pad(X, resize, resize) if resize else X,
-        y,
-    )
-    return (
-        tfds.as_numpy(
-            tf.data.Dataset.from_tensor_slices(process(*mnist_train))
-            .batch(batch_size)
-            .shuffle(len(mnist_train[0]))
-            .map(resize_fn)
-        ),
-        tfds.as_numpy(
-            tf.data.Dataset.from_tensor_slices(process(*mnist_test))
-            .batch(batch_size)
-            .map(resize_fn)
-        ),
-    )
+# def load_data_fashion_mnist(batch_size, resize=None):  # @save
+#     """Download the Fashion-MNIST dataset and then load it into memory."""
+#     mnist_train, mnist_test = tf.keras.datasets.fashion_mnist.load_data()
+#     # Divide all numbers by 255 so that all pixel values are between
+#     # 0 and 1, add a batch dimension at the last. And cast label to int32
+#     process = lambda X, y: (tf.expand_dims(X, axis=3) / 255, tf.cast(y, dtype="int32"))
+#     resize_fn = lambda X, y: (
+#         tf.image.resize_with_pad(X, resize, resize) if resize else X,
+#         y,
+#     )
+#     return (
+#         tfds.as_numpy(
+#             tf.data.Dataset.from_tensor_slices(process(*mnist_train))
+#             .batch(batch_size)
+#             .shuffle(len(mnist_train[0]))
+#             .map(resize_fn)
+#         ),
+#         tfds.as_numpy(
+#             tf.data.Dataset.from_tensor_slices(process(*mnist_test))
+#             .batch(batch_size)
+#             .map(resize_fn)
+#         ),
+#     )
