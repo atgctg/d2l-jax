a24a036b9760ec8e441954723ead29dc422fcf24
atgctg <105969161+atgctg@users.noreply.github.com>
2022-07-16 23:47:06 +0200

    comment out

--- mock_d2l_jax.py
+++ mock_d2l_jax.py
@@ -187,7 +187,7 @@ class Module(nn.Module, d2l.HyperParameters):
         assert hasattr(self, 'net'), 'Neural network is defined'
         return self.net(X)
 
-    def call(self, X, *args, **kwargs):
+    def __call__(self, X, *args, **kwargs):
         if kwargs and "training" in kwargs:
             self.training = kwargs['training']
         return self.forward(X, *args)
@@ -207,10 +207,11 @@ class Module(nn.Module, d2l.HyperParameters):
                 self.plot_valid_per_epoch
         self.board.draw(x, value, (
             'train_' if train else 'val_') + key, every_n=int(n))
+
     def training_step(self, batch):
-        l = self.loss(self(*batch[:-1]), batch[-1])
+        l, g = jax.value_and_grad(self.loss)(self(*batch[:-1]), batch[-1])
         self.plot('loss', l, train=True)
-        return l
+        return l, g
 
     def validation_step(self, batch):
         l = self.loss(self(*batch[:-1]), batch[-1])
@@ -245,74 +246,94 @@ class DataModule(d2l.HyperParameters):
             buffer_size=shuffle_buffer).batch(self.batch_size))
     
 
-class Trainer(d2l.HyperParameters):
-    """Defined in :numref:`sec_oo-design`"""
-    def __init__(self, max_epochs, num_gpus=0, gradient_clip_val=0):
-        self.save_hyperparameters()
-        assert num_gpus == 0, 'No GPU support yet'
+# class Trainer(d2l.HyperParameters):
+#     """Defined in :numref:`sec_oo-design`"""
+#     def __init__(self, max_epochs, num_gpus=0, gradient_clip_val=0):
+#         self.save_hyperparameters()
+#         assert num_gpus == 0, 'No GPU support yet'
 
-    def prepare_data(self, data):
-        self.train_dataloader = data.train_dataloader()
-        self.val_dataloader = data.val_dataloader()
-        self.num_train_batches = len(self.train_dataloader)
-        self.num_val_batches = (len(self.val_dataloader)
-                                if self.val_dataloader is not None else 0)
+#     def prepare_data(self, data):
+#         self.train_dataloader = data.train_dataloader()
+#         self.val_dataloader = data.val_dataloader()
+#         self.num_train_batches = len(self.train_dataloader)
+#         self.num_val_batches = (len(self.val_dataloader)
+#                                 if self.val_dataloader is not None else 0)
 
-    def prepare_model(self, model):
-        model.trainer = self
-        model.board.xlim = [0, self.max_epochs]
-        self.model = model
+#     def prepare_model(self, model):
+#         model.trainer = self
+#         model.board.xlim = [0, self.max_epochs]
+#         self.model = model
 
-    def fit(self, model, data):
-        self.prepare_data(data)
-        self.prepare_model(model)
-        self.optim = model.configure_optimizers()
-        self.epoch = 0
-        self.train_batch_idx = 0
-        self.val_batch_idx = 0
-        for self.epoch in range(self.max_epochs):
-            self.fit_epoch()
+#     def fit(self, model, data):
+#         self.prepare_data(data)
+#         self.prepare_model(model)
+#         self.optim = model.configure_optimizers()
+#         self.epoch = 0
+#         self.train_batch_idx = 0
+#         self.val_batch_idx = 0
+#         for self.epoch in range(self.max_epochs):
+#             self.fit_epoch()
 
-    def fit_epoch(self):
-        raise NotImplementedError
+#     def fit_epoch(self):
+#         raise NotImplementedError
 
-    def prepare_batch(self, batch):
-        """Defined in :numref:`sec_linear_scratch`"""
-        return batch
+#     def prepare_batch(self, batch):
+#         """Defined in :numref:`sec_linear_scratch`"""
+#         return batch
 
-    # todo + oo-design
-    def fit_epoch(self):
-        """Defined in :numref:`sec_linear_scratch`"""
-        self.model.training = True
-        for batch in self.train_dataloader:
-            with tf.GradientTape() as tape:
-                loss = self.model.training_step(self.prepare_batch(batch))
-            grads = tape.gradient(loss, self.model.trainable_variables)
-            if self.gradient_clip_val > 0:
-                grads = self.clip_gradients(self.gradient_clip_val, grads)
-            self.optim.apply_gradients(zip(grads, self.model.trainable_variables))
-            self.train_batch_idx += 1
-        if self.val_dataloader is None:
-            return
-        self.model.training = False
-        for batch in self.val_dataloader:
-            self.model.validation_step(self.prepare_batch(batch))
-            self.val_batch_idx += 1
+#     def fit_epoch(self):
+#         self.model.training = True
+#         for batch in self.train_dataloader:
+#             _, grads = self.model.training_step(self.prepare_batch(batch))
+#             if self.gradient_clip_val > 0:
+#                 grads = self.clip_gradients(self.gradient_clip_val, grads)
+#             self.optim.apply_gradients(grads)
+#             self.train_batch_idx += 1
+#         if self.val_dataloader is None:
+#             return
+#         self.model.training = False
+#         for batch in self.val_dataloader:
+#             self.model.validation_step(self.prepare_batch(batch))
+#             self.val_batch_idx += 1
 
-    # todo
-    def clip_gradients(self, grad_clip_val, grads):
-        """Defined in :numref:`sec_rnn-scratch`"""
-        grad_clip_val = tf.constant(grad_clip_val, dtype=tf.float32)
-        new_grads = [tf.convert_to_tensor(grad) if isinstance(
-            grad, tf.IndexedSlices) else grad for grad in grads]
-        norm = tf.math.sqrt(sum((tf.reduce_sum(grad ** 2)) for grad in new_grads))
-        if tf.greater(norm, grad_clip_val):
-            for i, grad in enumerate(new_grads):
-                new_grads[i] = grad * grad_clip_val / norm
-            return new_grads
+#     # todo
+#     def clip_gradients(self, grad_clip_val, grads):
+#         """Defined in :numref:`sec_rnn-scratch`"""
+#         grad_clip_val = tf.constant(grad_clip_val, dtype=tf.float32)
+#         new_grads = [tf.convert_to_tensor(grad) if isinstance(
+#             grad, tf.IndexedSlices) else grad for grad in grads]
+#         norm = tf.math.sqrt(sum((tf.reduce_sum(grad ** 2)) for grad in new_grads))
+#         if tf.greater(norm, grad_clip_val):
+#             for i, grad in enumerate(new_grads):
+#                 new_grads[i] = grad * grad_clip_val / norm
+#             return new_grads
         return grads
 
 # 3.3
+
+class SyntheticRegressionData(d2l.DataModule):  #@save
+    def __init__(self, key, w, b, noise=0.01, num_train=1000, num_val=1000,
+                 batch_size=32):
+        super().__init__()
+        self.save_hyperparameters()
+        n = num_train + num_val
+        key1, key2 = jax.random.split(key, 2)
+        self.X = jax.random.normal(key1, (n, len(w)))
+        noise = jax.random.normal(key2, (n, 1)) * noise
+        self.y = jnp.matmul(self.X, w.reshape((-1, 1))) + b + noise
+
+# Use TensorFlow's dataloader, since JAX doesn't have one
+@d2l.add_to_class(d2l.DataModule)  #@save
+def get_tensorloader(self, tensors, train, indices=slice(0, None)):
+    tensors = tuple(a[indices] for a in tensors)
+    shuffle_buffer = tensors[0].shape[0] if train else 1
+    return tfds.as_numpy(tf.data.Dataset.from_tensor_slices(tensors).shuffle(
+        buffer_size=shuffle_buffer).batch(self.batch_size))
+
+@d2l.add_to_class(SyntheticRegressionData)  #@save
+def get_dataloader(self, train):
+    i = slice(0, self.num_train) if train else slice(self.num_train, None)
+    return self.get_tensorloader((self.X, self.y), train, i)
 
 # Since JAX doesn't have a dataloader, we'll use PyTorch's
 # def load_array(data_arrays, batch_size, is_train=True):  # @save
