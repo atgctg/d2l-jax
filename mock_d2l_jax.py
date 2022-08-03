0e3bc52406d64380e5c8da8059a21a18c7443136
atgctg <105969161+atgctg@users.noreply.github.com>
2022-08-03 17:20:16 +0200

    5.7

--- mock_d2l_jax.py
+++ mock_d2l_jax.py
@@ -1,6 +1,13 @@
+DATA_HUB = dict()
+DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
+
+import os
 import sys
 import collections
 import inspect
+import zipfile
+import requests
+import hashlib
 import time
 from IPython import display
 from dataclasses import field
@@ -535,6 +542,45 @@ class Classifier(d2l.Module):  #@save
     #         X = layer(X)
     #         print(layer.__class__.__name__, 'output shape:\t', X.shape)
 
+# 5.7
+
+def download(url, folder='../data', sha1_hash=None):  #@save
+    """Download a file to folder and return the local filepath."""
+    if not url.startswith('http'):
+        # For back compatability
+        url, sha1_hash = DATA_HUB[url]
+    os.makedirs(folder, exist_ok=True)
+    fname = os.path.join(folder, url.split('/')[-1])
+    # Check if hit cache
+    if os.path.exists(fname) and sha1_hash:
+        sha1 = hashlib.sha1()
+        with open(fname, 'rb') as f:
+            while True:
+                data = f.read(1048576)
+                if not data:
+                    break
+                sha1.update(data)
+        if sha1.hexdigest() == sha1_hash:
+            return fname
+    # Download
+    print(f'Downloading {fname} from {url}...')
+    r = requests.get(url, stream=True, verify=True)
+    with open(fname, 'wb') as f:
+        f.write(r.content)
+    return fname
+
+def extract(filename, folder=None):  #@save
+    """Extract a zip/tar file into folder."""
+    base_dir = os.path.dirname(filename)
+    _, ext = os.path.splitext(filename)
+    assert ext in ('.zip', '.tar', '.gz'), 'Only support zip/tar files.'
+    if ext == '.zip':
+        fp = zipfile.ZipFile(filename, 'r')
+    else:
+        fp = tarfile.open(filename, 'r')
+    if folder is None:
+        folder = base_dir
+    fp.extractall(folder)
 
 # 6.7
 
